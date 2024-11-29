0dbba6e65dd0081b89fa65a406609fe535333a23
Madhugit15 <madhushree1530@gmail.com>
2024-11-29 16:40:04 +0530

    index.html index.css index.js files added

---
+++ index.js
@@ -0,0 +1,29 @@
+const search=document.querySelector("#search-icon");
+console.log(search);
+const searchBar= document.querySelector(".search-bar");
+console.log(searchBar)
+
+search.addEventListener("click", function(e){
+   
+   searchBar.classList.toggle("active")
+   menuBar.classList.remove("navs");
+   
+})
+
+
+let navBar=document.querySelector("#nav-bar");
+console.log(navBar);
+let menuBar=document.querySelector(".link")
+console.log(menuBar);
+
+navBar.addEventListener("click",function(){
+
+   menuBar.classList.toggle("navs");
+   searchBar.classList.remove("active")
+  
+})
+
+window.onscroll=()=>{
+   
+   menuBar.classList.remove("navs");
+}
